f8b94c502cfccd2b20def7fed3795d505148c9d4
Vinnie <vincent.king1224@gmail.com>
2017-07-07 14:38:38 -0700

    api rest operations fully functional

--- server.js
+++ server.js
@@ -12,17 +12,46 @@ app.post('/notes', (req, res) => {
 })
 
 app.get('/notes', (req, res) => {
+
     const query = {order:[['updatedAt', 'DESC']], where: {id: {$gte: 1}}};
+
     if (req.params.limit) {
         query.limit = req.params.limit
     };
+
     if (req.params.order) {
         query. order = [['updatedAt', req.params.order]]
     };
+
     if (req.params.start) {
         query.where = {id: {$gte: req.params.start}}
     }
+
     Note.findAll(query).then((notes)=>{res.json(notes)})
+})
+
+app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    Note.findById(id).then((note)=>{
+        res.json(note)
+    })
+})
+
+app.put('/notes/:id', (req,res)=> {
+    Note.update({ data: req.body.data},{ where: { id: req.params.id }})
+    .then(()=>{
+        Note.findById(req.params.id)
+        .then((note)=>{
+            res.json(note);
+        })
+    })
+})
+
+app.delete('/notes/:id', (req,res)=> {
+    Note.destroy({ where: { id: req.params.id }})
+    .then(()=>{
+        res.end()
+    })
 })
 
 app.listen(3000, function(){
